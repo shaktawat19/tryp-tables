1361572ce4cb17a687e6c903e0ac632a0e71915b
shaktawat19 <harshshaktawat.tech@gmail.com>
2023-07-15 16:14:57 +0530

    Initial setup and sorting of data

---
+++ src/components/DataTable.tsx
@@ -0,0 +1,102 @@
+import React, { useMemo } from "react";
+import { useTable, useSortBy } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { format } from "date-fns";
+
+const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+    Footer: "Id",
+    disableFilters: true,
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+    Footer: "First Name",
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+    Footer: "Last Name",
+  },
+  {
+    Header: "Date of Birth",
+    accessor: "date_of_birth",
+    Footer: "Date of Birth",
+    Cell: ({ value }) => {
+      return format(new Date(value), "dd/MM/yyyy");
+    },
+  },
+  {
+    Header: "Country",
+    accessor: "country",
+    Footer: "Country",
+  },
+  {
+    Header: "Phone",
+    accessor: "phone",
+    Footer: "Phone",
+  },
+];
+
+const DataTable = (props) => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      disableSortRemove: true,
+    },
+    useSortBy
+  );
+
+  return (
+    <div className="table-container">
+      <h2>Table</h2>
+
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? "🔽" : "🔼") : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import DataTable from '@/components/DataTable';
+import classes from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={classes.container}>
+      <DataTable />
+    </div>
+  );
+}
